6009e00e44d83b97978098cd6214aa486f3ebc0b
Tasha Hankewych <tash1207@bellsouth.net>
2017-10-09 00:53:06 -0700

    Start reading values from MongoDB and use them in the ejs template.

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 var cool = require('cool-ascii-faces');
 var express = require('express');
+var mongodb = require('mongodb');
+
 var app = express();
+var MongoClient = mongodb.MongoClient;
+
+// Mongo variables
+var db;
+var url = process.env.MONGODB_URI;
 
 // Set the port of our application.
 // process.env.PORT lets the port be set by Heroku.
@@ -12,18 +19,33 @@ app.set('view engine', 'ejs');
 // Make express look in the public directory for assets (css/js/img).
 app.use(express.static(__dirname + '/public'));
 
+MongoClient.connect(url, function (err, database) {
+  if (err) {
+    console.log('Unabled to connect to mongoDB. Error:', err);
+  } else {
+    console.log('Connection established to', url);
+
+    db = database;
+
+    // Only listen if the db is connected.
+    app.listen(port, function() {
+      console.log('App is running on http://localhost:' + port);
+    });
+  }
+});
+
 // Set the home page route.
 app.get('/', function(req, res) {
-  // ejs render automatically looks in the views folder.
-  res.render('index');
+  // Get the info from the database.
+  var col = db.collection('reviews');
+  col.find().toArray(function(err, reviews) {
+    // ejs render automatically looks in the views folder.
+    res.render('index', {reviews: reviews});
+  });
 });
 
 // Return a cool ascii face on the /cool route.
 app.get('/cool', function(req, res) {
   res.send(cool());
-});
-
-app.listen(port, function() {
-  console.log('App is running on http://localhost:' + port);
 });
 
